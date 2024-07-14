7dc795b73acb89f9b597380eeb8b8a093275ceb7
IvanRuskevych <ivan.ruskevych@gmail.com>
2024-07-14 15:50:06 +0300

    routerLink for /dash & /agents works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AgentComponent } from './components/agent/agent.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'dash', component: DashboardComponent },
+  { path: 'agents', component: AgentComponent },
+];
