11b6b25b532662ae23ef032b28b0dea100187299
IvanRuskevych <ivan.ruskevych@gmail.com>
2024-07-21 20:57:20 +0300

    add interseptor settings to app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,24 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { MatNativeDateModule } from '@angular/material/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-import { MatNativeDateModule } from '@angular/material/core';
 
 import { routes } from './app.routes';
+import { TokenInterceptorService } from './services';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
     provideAnimationsAsync(),
     importProvidersFrom(MatNativeDateModule),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true,
+    },
   ],
 };
