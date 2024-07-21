d85293e2aecc5fdcdd5431aab95402b952bebd85
IvanRuskevych <ivan.ruskevych@gmail.com>
2024-07-22 00:34:06 +0300

    create current user service

--- src/app/services/user-api.service.ts
+++ src/app/services/user-api.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { IUser } from '../models/user.model';
+import { apiEndpoints } from '../constants';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserApiService {
+  private readonly apiUrl: string = environment.apiUrl;
 
   constructor(private http: HttpClient) {
+  }
+
+  public getUser(): Observable<IUser> {
+    return this.http.get<IUser>(`${this.apiUrl}${apiEndpoints.USER_CURRENT}`);
   }
 }
